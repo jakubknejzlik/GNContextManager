e6eb7dcaf83ed969dc0079dd353deb16e0bcd393
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2015-01-26 17:34:23 +0100

    old methods removed

--- GNContextManager/GNContextManager.h
+++ GNContextManager/GNContextManager.h
@@ -23,26 +23,4 @@
 -(NSManagedObjectContext *)managedObjectContextWithSettings:(GNContextSettings *)settings;
 
 
-#pragma DEPRECATE methods from old API
-@property (nonatomic,copy) NSString *defaultManagedObjectContextKey DEPRECATED_ATTRIBUTE;
-@property (nonatomic,copy) NSString *managedObjectModelPath DEPRECATED_ATTRIBUTE;
-@property (nonatomic,copy) NSURL *persistentStoreURL DEPRECATED_ATTRIBUTE;
-@property (nonatomic,copy) NSString *persistentStoreType DEPRECATED_ATTRIBUTE;
-
--(instancetype)initWithModelPath:(NSString *)modelPath persistentStoreURL:(NSURL *)persistentStoreURL persistentStoreType:(NSString *)persistentStoreType DEPRECATED_ATTRIBUTE;
-
--(NSManagedObjectContext *)defaultManagedObjectContext DEPRECATED_ATTRIBUTE;
-
--(NSManagedObjectContext *)temporaryManagedObjectContext DEPRECATED_ATTRIBUTE;
--(NSManagedObjectContext *)temporaryManagedObjectContextWithConcurrencyType:(NSManagedObjectContextConcurrencyType)concurrencyType DEPRECATED_ATTRIBUTE;
--(NSManagedObjectContext *)managedObjectContextForKey:(NSString *)key DEPRECATED_ATTRIBUTE;
--(NSManagedObjectContext *)managedObjectContextForKey:(NSString *)key concurrencyType:(NSManagedObjectContextConcurrencyType)concurrencyType DEPRECATED_ATTRIBUTE;
-
--(NSManagedObjectContext *)managedObjectContextForKey:(NSString *)key modelPath:(NSString *)modelPath persistentStoreURL:(NSURL *)persistentStoreURL persistentStoreType:(NSString *)persistentStoreType concurrencyType:(NSManagedObjectContextConcurrencyType)concurrencyTypeDEPRECATED_ATTRIBUTE;
-
--(void)flush DEPRECATED_ATTRIBUTE;
-
--(void)destroyDefaultManagedObjectContext DEPRECATED_ATTRIBUTE;
--(void)destroyManagedObjectContextForKey:(NSString *)key DEPRECATED_ATTRIBUTE;
-
 @end
